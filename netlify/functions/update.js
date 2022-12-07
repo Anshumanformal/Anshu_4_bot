b31fe4246c24dd93924a67cb142b29b5d604b68e
Anshuman Ranjan <anshumanranjan1998@gmail.com>
2022-12-07 10:19:08 +0000

    Respond to messages

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,3 +1,14 @@
+const axios = require("axios").default;
+const url = "https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage"
+
+await axios.post(url, {
+  // TODO: Add form data
+  chat_id: JSON.parse(event.body).message.chat.id,
+  text: "I got your message!",
+});
+
+
+
 exports.handler = async (event) => {
     console.log("Received an update from Telegram!", event.body);
     return { statusCode: 200 };
