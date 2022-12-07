f0a5e64f2fae71e5f0bc053b6ff9b3d4f75bbaff
Anshuman Ranjan <anshumanformal@gmail.com>
2022-12-07 17:09:54 +0530

    testing

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -2,6 +2,7 @@ const sendMessage = require("../../sendMessage");
 
 exports.handler = async (event) => {
   const { message } = JSON.parse(event.body);
+  console.log('event body-----', event.body)
   await sendMessage(message.chat.id, "I got your message!");
   return { statusCode: 200 };
 };
